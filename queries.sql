b32d2a0681ab828129f305d6c4e58e6241ee2478
Lithlok <Joncorrell_BC@gmail.com>
2021-07-31 23:41:27 -0500

    Post Deliverable 2 commit

--- queries.sql
+++ queries.sql
@@ -146,3 +146,26 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
+
+
+-- Create Retirement Title table
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no ASC;
+
+DROP TABLE retirement_titles;
+
+- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no ASC, to_date DESC;
